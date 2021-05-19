58b4bbe819e97fc68ac1f257639ef1f5e302a160
BattleWoofer <omeet100@gmail.com>
2021-05-19 17:14:32 -0500

    bb

--- js/layers.js
+++ js/layers.js
@@ -44,7 +44,7 @@ addLayer("p", {
             cost: new Decimal(10),
 
             effect() {
-               eff = player.p.points.add(1).log(5).add(5).pow(2).div(4);
+               eff = new Decimal(1.1).pow(player.p.points.pow).add(1);
                return eff
             },
             effectDisplay() { return format(tmp.p.upgrades[12].effect)+"x" },
