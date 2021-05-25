2792dfe0b7108ed510969101a456f5c2b3cea6a2
BattleWoofer <omeet100@gmail.com>
2021-05-24 22:19:30 -0500

    canbuymax

--- js/layers.js
+++ js/layers.js
@@ -258,6 +258,7 @@ addLayer("s", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    canBuyMax(){return true},
     exponent: 0.33, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
