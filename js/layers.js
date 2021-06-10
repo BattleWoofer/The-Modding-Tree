027e5565872cad0eaf663e6f0803d770b1954e93
BattleWoofer <omeet100@gmail.com>
2021-06-10 15:41:57 -0500

    br

--- js/layers.js
+++ js/layers.js
@@ -382,7 +382,7 @@ addLayer("s", {
         let softcap = this.seff
         eff = Decimal.add(player.s.points, 1).pow(0.25)
         //if (eff.gte(18)) eff = new Decimal(18).add(player.s.points.sub(104976).pow(softcap)).sub(1)
-        eff = softbase
+        eff = softcap
         return eff
     },
     effectDescription() {
